218e3a912dbaeb66005241d1b3810a44ec634d9f
Denis Baryshev <dennybaa@gmail.com>
2016-10-26 18:29:23 +0300

    use ip command to find address number

--- ipaddr.sh
+++ ipaddr.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 # Display ip address of the given number (default: 1), Serf query payload passed as STDIN
 
+ip_addresses() {
+    ip address | grep 'inet\s' | grep -v '\blo\b' | sed -n 's/.*inet \(.*\)\/.*/\1/;p'
+}
+
 read addrnum
-hostnames -I | cut -f${addrnum} -d' '
+ip_addresses | sed "${addrnum}!d"
